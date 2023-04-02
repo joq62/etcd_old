d7ddb4d14e3fad330e3fd3081ebe4662ee8af4ae
joq62 <jakim.leche@gmail.com>
2023-04-02 20:52:20 +0200

    ud

--- tests/provider_tests.erl
+++ tests/provider_tests.erl
@@ -44,26 +44,28 @@ read_specs_test(Node)->
     AllProviders=lists:sort(rpc:call(Node,db_provider_spec,get_all_id,[],5000)),
     true=lists:member("kube",AllProviders),
 
-    {"kube","kube","0.1.0","kube","kube","a_cookie",
-     {os,cmd,["git clone https://github.com/joq62/kube.git -C kube"]},
-     {os,cmd,["tar -xvf kube/kube-0.1.0.tar.gz -C kube "]},
-     {os,cmd,["./kube/bin/kube daemon"]},
-     1,
-     [all_hosts]
-    }=rpc:call(Node,db_provider_spec,read,["kube"],5000),
+   {
+    "kube","kube","0.1.0","kube",kube,"kube","kube","a_cookie",
+    "https://github.com/joq62/kube.git"," -pa -pa kube/lib/*/* ",
+    "tar -xvf kube/kube-0.1.0.tar.gz -C kube ",
+    {application,start,[kube],20000},1,[all_hosts]
+   }=rpc:call(Node,db_provider_spec,read,["kube"],5000),
     
     {ok,"kube"}=rpc:call(Node,db_provider_spec,read,[spec,"kube"],5000),
     {ok,"kube"}=rpc:call(Node,db_provider_spec,read,[appl_name,"kube"],5000),
     {ok,"0.1.0"}=rpc:call(Node,db_provider_spec,read,[vsn,"kube"],5000),
+    {ok,"kube"}=rpc:call(Node,db_provider_spec,read,[app_name,"kube"],5000),
+    {ok,kube}=rpc:call(Node,db_provider_spec,read,[app,"kube"],5000),
     {ok,"kube"}=rpc:call(Node,db_provider_spec,read,[dir,"kube"],5000),
     {ok,"kube"}=rpc:call(Node,db_provider_spec,read,[node_name,"kube"],5000),
     {ok,"a_cookie"}=rpc:call(Node,db_provider_spec,read,[cookie,"kube"],5000),
-    {ok,{os,cmd,["git clone https://github.com/joq62/kube.git -C kube"]}}=rpc:call(Node,db_provider_spec,read,[clone_cmd,"kube"],5000),
-    {ok,{os,cmd,["tar -xvf kube/kube-0.1.0.tar.gz -C kube "]}}=rpc:call(Node,db_provider_spec,read,[tar_cmd,"kube"],5000),
-    {ok,{os,cmd,["./kube/bin/kube daemon"]}}=rpc:call(Node,db_provider_spec,read,[start_cmd,"kube"],5000),
+    {ok," -pa -pa kube/lib/*/* "}=rpc:call(Node,db_provider_spec,read,[pa_args,"kube"],5000),
+    {ok,"https://github.com/joq62/kube.git"}=rpc:call(Node,db_provider_spec,read,[git_path,"kube"],5000),
+    {ok,"tar -xvf kube/kube-0.1.0.tar.gz -C kube "}=rpc:call(Node,db_provider_spec,read,[tar_cmd,"kube"],5000),
+    {ok,{application,start,[kube],20000}}=rpc:call(Node,db_provider_spec,read,[start_cmd,"kube"],5000),
     {ok,1}=rpc:call(Node,db_provider_spec,read,[num,"kube"],5000),
     {ok, [all_hosts]}=rpc:call(Node,db_provider_spec,read,[affinity,"kube"],5000),
-
+    
  
     {error,[eexist,"glurk",db_provider_spec,_]}=rpc:call(Node,db_provider_spec,read,[dir,"glurk"],5000),
     {error,['Key eexists',glurk,"kube",db_provider_spec,_]}=rpc:call(Node,db_provider_spec,read,[glurk,"kube"],5000),
